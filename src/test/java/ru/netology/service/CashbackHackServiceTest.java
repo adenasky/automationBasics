a0714af6051a16003546779a2350e55059570562
adenasky <adenasky@gmail.com>
2023-11-27 22:39:46 +0300

    add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainAmountLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainAmountMoreThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1200;
+
+        int actual = service.remain(amount);
+        int expected = 800;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+}
